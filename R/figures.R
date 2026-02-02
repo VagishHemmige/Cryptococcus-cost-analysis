9105fa6e81d5ba0c6d375d0205f1762bfea3f9e8
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-01 19:44:22 -0500

    Comments

--- R/figures.R
+++ R/figures.R
@@ -6,7 +6,7 @@ patients_fc_matched%>%
   fc_draw()
 
 
-#GGplot
+#GGplot that is beeswarm of adjusted total costs
 cost_inflated%>%
   mutate(IN_CLM_365d_cost_adjusted_total=ifelse(IN_CLM_365d_cost_adjusted_total==0, 1, IN_CLM_365d_cost_adjusted_total))%>%
   ggplot()+
@@ -23,9 +23,14 @@ emm_df_IN_CLM <- emmeans(
   fit_IN_CLM,
   ~ patient_type | month,
   type = "response"
-)%>%as.data.frame()
+)%>%as.data.frame()%>%
+  mutate(month=0.5+as.numeric(month))
 
 #Now, use ggplot to pl
 ggplot(data=emm_df_IN_CLM)+
   geom_point(mapping=aes(x=month, y=response, color=patient_type))+
-  geom_errorbar(aes(x=month, ymin = asymp.LCL, ymax = asymp.UCL, color=patient_type))
+  geom_errorbar(aes(x=month, ymin = asymp.LCL, ymax = asymp.UCL, color=patient_type))+
+  geom_vline(xintercept = 0, linetype = "dashed", color = "gray50")+
+  theme_classic()+
+  theme(legend.position = "bottom")+
+  labs(x="Month", y="Inflation-adjusted cost per month (dollars)", color = "Patient group")
